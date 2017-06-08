bc0e046a22bf0c19d5cb63a84f91af3cd9adc0f5
Daniel Löfquist <daniel@lofquist.org>
2017-06-08 23:33:03 +0200

    Clean-up and tweaks

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bara-midlara
 A tiny server that serves up static JSON from files. "Bara miðlara" is Icelandic and means "just a server".
+
+## Build with Maven
+```
+mvn clean package
+```
+
+## Run
+```
+java -Dapi.config=demo/demo.config -jar target/bara-midlara.jar
+```
+Go to http://localhost:8080/api/this or http://localhost:8080/another-api/that to see the demo running.
--- src/main/java/org/dirtylabcoat/baramidlara/App.java
+++ src/main/java/org/dirtylabcoat/baramidlara/App.java
@@ -34,18 +34,19 @@ public class App {
     }
 
     public static class ApiServlet extends HttpServlet {
-	private static final long serialVersionUID = -6154475791000019575L;
+	private static final long serialVersionUID = -6154475791066619575L;
         private static final Logger LOGGER = Logger.getLogger(ApiServlet.class.getName());
+
         private final Map<String, String> apis = new HashMap<>();
 
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,	IOException {
-            String apiFile = request.getRequestURI();
-            if (apis.containsKey(apiFile)) {
-                String fileString = new String(Files.readAllBytes(Paths.get(apis.get(apiFile))), StandardCharsets.UTF_8);
+            String requestedApi = request.getRequestURI();
+            if (apis.containsKey(requestedApi)) {
+                String json = new String(Files.readAllBytes(Paths.get(apis.get(requestedApi))), StandardCharsets.UTF_8);
                 response.setContentType("application/json");
                 response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().println(fileString);
+                response.getWriter().println(json);
             } else {
                 response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             }
@@ -53,17 +54,17 @@ public class App {
 
         @Override
         public void init() {
-            String apiFilename = System.getProperty("api.file");
+            String configFile = System.getProperty("api.config");
             Properties properties = new Properties();
             try {
-                try (InputStream is = new FileInputStream(apiFilename)) {
+                try (InputStream is = new FileInputStream(configFile)) {
                     properties.load(is);
                     properties.entrySet().stream().forEach((entry) -> {
                         apis.put(entry.getKey().toString(), entry.getValue().toString());
                     });
                 }
             } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, "Unable to read configuration file.");
+                LOGGER.log(Level.SEVERE, "Unable to read configuration file. [{0}]", e.toString());
             }
         }
 
